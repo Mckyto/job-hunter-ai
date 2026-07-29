9497b4cefed7a28363773091c000bd532330a978
Mckyto <smihaita25@gmail.com>
2026-07-30 02:09:52 +0300

    Imbunatatire scoring si configurare joburi

--- app/scoring.py
+++ app/scoring.py
@@ -9,23 +9,72 @@ class JobScorer:
         score = 0
 
         title = job.title.lower()
+        location = job.location.lower()
 
-        # Domeniu potrivit
-        for keyword in self.config["keywords"]:
-            if keyword.lower() in title:
-                score += 40
+
+        # Prioritate mare: joburi dorite
+        high_priority = [
+            "customer support",
+            "customer service",
+            "chat support",
+            "chat agent",
+            "data entry",
+            "operator date",
+            "back office"
+        ]
+
+
+        # Prioritate medie
+        medium_priority = [
+            "call center",
+            "operator call center",
+            "suport clienti",
+            "consultant vanzari",
+            "lucrator comercial"
+        ]
+
+
+        # Domenii bonus
+        bonus_keywords = [
+            "beauty",
+            "cosmetice",
+            "machiaj",
+            "makeup",
+            "mall",
+            "magazin",
+            "tutungerie"
+        ]
+
+
+        for keyword in high_priority:
+
+            if keyword in title:
+                score += 50
                 break
 
 
-        # Locație potrivită
-        for location in self.config["locations"]:
-            if location.lower() in job.location.lower():
-                score += 30
+        for keyword in medium_priority:
+
+            if keyword in title:
+                score += 35
                 break
+
+
+        for keyword in bonus_keywords:
+
+            if keyword in title:
+                score += 25
+                break
+
+
+        # Remote este un avantaj
+        if "remote" in location:
+            score += 20
 
 
         # Salariu
         if job.salary:
+
             salary = int(
                 "".join(
                     filter(str.isdigit, job.salary)
