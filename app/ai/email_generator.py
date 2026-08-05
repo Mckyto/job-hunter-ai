1427db2804abab50778f7637a15e09a559e4bee8
Mckyto <155581277+Mckyto@users.noreply.github.com>
2026-08-05 19:02:21 +0300

    Update email_generator.py

--- app/ai/email_generator.py
+++ app/ai/email_generator.py
@@ -13,6 +13,6 @@ class EmailGenerator:
             f"Sunt o persoană motivată, cu experiență în suport și relații cu clienții, "
             f"pregătită să aduc valoare echipei dumneavoastră.\n\n"
             f"Atașat găsiți CV-ul meu pentru mai multe detalii.\n\n"
-            f"O zi excelentă,\nEchipa / dremaer"
+            f"O zi excelentă,\nGabriela Usurelu"
         )
         return email_content
